1c92bbddec20df74a658ef08e6209d39c302c3d3
devesh1806 <42887196+devesh1806@users.noreply.github.com>
2019-12-26 18:29:15 +0530

    Final edit Done

--- backend/api/db.py
+++ backend/api/db.py
@@ -8,14 +8,6 @@ mysql = MySQL()
 app.config["MYSQL_DATABASE_USER"] = "root"
 # app.config["MYSQL_DATABASE_PASSWORD"] = os.environ["MYSQL_ROOT_DB_PASSWORD"]
 app.config["MYSQL_DATABASE_PASSWORD"] = 'root'
-<<<<<<< HEAD
 app.config["MYSQL_DATABASE_DB"] = "sepsis"
-=======
-<<<<<<< HEAD
-app.config["MYSQL_DATABASE_DB"] = "missing"
-=======
-app.config["MYSQL_DATABASE_DB"] = "sepsis"
->>>>>>> f5e817d1755102841bdaf84590d780aec04355ea
->>>>>>> dceab5d1b26e5089a4b177100beb25840d24ec94
 app.config["MYSQL_DATABASE_HOST"] = "localhost"
 mysql.init_app(app)
